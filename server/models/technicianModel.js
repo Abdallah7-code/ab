666f0dafef9d987c13be2cc30c296a313f1a4912
اسمك <@>
2025-07-30 16:09:16 +0400

    Add technician model

---
+++ server/models/technicianModel.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const technicianSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  phone: String,
+  email: String,
+  department: String,
+  ratings: [Number]
+}, { timestamps: true });
+
+module.exports = mongoose.model('Technician', technicianSchema);
